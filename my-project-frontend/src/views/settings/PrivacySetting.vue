19a14c040246de4bba9006fba899866c33164394
M200 <1318003424@qq.com>
2023-10-17 00:13:11 +0800

    用户隐私部分编写完成，需要测试

--- my-project-frontend/src/views/settings/PrivacySetting.vue
+++ my-project-frontend/src/views/settings/PrivacySetting.vue
@@ -3,7 +3,7 @@
 import Card from "@/components/Card.vue";
 import {Lock, Setting, Switch} from "@element-plus/icons-vue";
 import {reactive, ref} from "vue";
-import {post} from "@/net";
+import {get, post} from "@/net";
 import {ElMessage} from "element-plus";
 
 const form=reactive({
@@ -49,6 +49,7 @@ function resetPassword(){
   })
 }
 
+const saving=ref(true)
 const privacy=reactive({
   phone: false,
   wx:false,
@@ -56,18 +57,41 @@ const privacy=reactive({
   email:false,
   gender:false
 })
+get('/api/user/privacy',data=>{
+  privacy.phone=data.phone
+  privacy.email=data.email
+  privacy.wx=data.wx
+  privacy.qq=data.qq
+  privacy.gender=data.gender
+  saving.value=false
+})
+function  savePrivacy(type,status){
+  saving.value=true;
+  post('/api/user/save-privacy',{
+    type:type,
+    status:status
+  },()=>{
+    ElMessage.success('隐私设置修改成功！')
+    saving.value=false
+  })
+}
 </script>
 
 <template>
   <div style="margin: auto;max-width: 600px">
     <div style="margin-top: 20px">
-      <card :icon="Setting" title="隐私设置" desc="在这里设置哪些内容可以被其他人看到">
+      <card :icon="Setting" title="隐私设置" desc="在这里设置哪些内容可以被其他人看到" v-loading="saving">
         <div class="checkbox-list">
-          <el-checkbox>公开展示我的手机号</el-checkbox>
-          <el-checkbox>公开展示我的电子邮件</el-checkbox>
-          <el-checkbox>公开展示我的微信号</el-checkbox>
-          <el-checkbox>公开展示我的QQ号</el-checkbox>
-          <el-checkbox>公开展示我的性别</el-checkbox>
+          <el-checkbox @change="savePrivacy('phone',privacy.phone)"
+                        v-model="privacy.phone">公开展示我的手机号</el-checkbox>
+          <el-checkbox @change="savePrivacy('email',privacy.email)"
+                       v-model="privacy.email">公开展示我的电子邮件</el-checkbox>
+          <el-checkbox @change="savePrivacy('wx',privacy.wx)"
+                       v-model="privacy.wx">公开展示我的微信号</el-checkbox>
+          <el-checkbox @change="savePrivacy('qq',privacy.qq)"
+                       v-model="privacy.qq">公开展示我的QQ号</el-checkbox>
+          <el-checkbox @change="savePrivacy('gender',privacy.gender)"
+                       v-model="privacy.gender">公开展示我的性别</el-checkbox>
         </div>
       </card>
       <card style="margin: 20px 0" :icon="Setting" title="修改密码" desc="修改密码在这里进行操作，请务必牢记密码">
