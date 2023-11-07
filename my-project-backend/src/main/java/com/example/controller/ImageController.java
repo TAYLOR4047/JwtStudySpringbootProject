136ceb6f950fa7d1ded7d14e59a6535a8613b611
M200 <1318003424@qq.com>
2023-11-07 23:36:52 +0800

    头像上传后端

---
+++ my-project-backend/src/main/java/com/example/controller/ImageController.java
@@ -0,0 +1,25 @@
+package com.example.controller;
+
+import com.example.entity.RestBean;
+import com.example.utils.Const;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/image")
+public class ImageController {
+
+    //存储用户头像不直接保存用户uid，而且采用随机数进行直接的保存
+    @PostMapping("/avatar")
+    public RestBean<String> uploadAvatar(@RequestParam("file")MultipartFile file,
+                                         @RequestAttribute(Const.ATTR_USER_ID)int id)throws IOException {
+        //if(file.getSize()>1025*100)
+            return RestBean.failure(400,"头像图片不能大于100KB");
+        //log.info("正在进行头像上传操作。。。");
+        //String url=
+    }
+}
